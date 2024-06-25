dfeb412ea2f89a7ea380a4664dd54c0ecbb01122
Jgier47 <giergielewiczjakub@gmail.com>
2024-06-25 11:04:33 +0200

    * Added CircuitBreaker with Retry pattern to method call to department service * Cleared application.properties to fully use config server * Added JSON NotNull annotation to DepartmentDTO * Added small logger info

--- src/main/java/net/java/app/employee_service/service/APIFeignClient.java
+++ src/main/java/net/java/app/employee_service/service/APIFeignClient.java
@@ -1,14 +1,26 @@
 package net.java.app.employee_service.service;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import net.java.app.employee_service.dto.DepartmentDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "DEPARTMENT-SERVICE")
-//@FeignClient(url = "http://localhost:8080",value = "DEPARTMENT-SERVICE")
 public interface APIFeignClient {
+  Logger LOGGER = LoggerFactory.getLogger(APIFeignClient.class);
 
+  @Retry(name = "DEPARTMENT-SERVICE")
+  @CircuitBreaker(name = "DEPARTMENT-SERVICE", fallbackMethod = "getDepartmentByCodeFallback")
   @GetMapping("api/departments/{department-code}")
   DepartmentDTO getDepartment(@PathVariable("department-code") String departmentCode);
+
+  default DepartmentDTO getDepartmentByCodeFallback(String departmentCode, Throwable exception) {
+    LOGGER.info("Searched department code not found: " + departmentCode);
+    LOGGER.info(exception.getLocalizedMessage());
+    return new DepartmentDTO(0L, "R&D", "R&D department", "RD13");
+  }
 }
--- src/main/java/net/java/app/employee_service/service/impl/EmployeeServiceImpl.java
+++ src/main/java/net/java/app/employee_service/service/impl/EmployeeServiceImpl.java
@@ -13,6 +13,8 @@ import net.java.app.employee_service.mapper.EmployeeMapper;
 import net.java.app.employee_service.repository.EmployeeRepository;
 import net.java.app.employee_service.service.APIFeignClient;
 import net.java.app.employee_service.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +23,7 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
   @Autowired private EmployeeRepository employeeRepository;
   @Autowired private EmployeeMapper employeeMapper;
   @Autowired private APIFeignClient apiFeignClient;
@@ -38,12 +41,20 @@ public class EmployeeServiceImpl implements EmployeeService {
 
   @Override
   public ResponseEmployeeDepartmentDTO getEmployeeById(Long employeeId) {
+
     Employee employee =
         employeeRepository
             .findById(employeeId)
             .orElseThrow(
                 () ->
                     new ResourceNotFoundException("Employee", "employeeID", employeeId.toString()));
+    LOGGER.info(
+        String.format(
+            "Gathered employee details: Employee first name %s, Employee Last name %s, Employee email %s, Employee department code %s",
+            employee.getFirstName(),
+            employee.getLastName(),
+            employee.getEmail(),
+            employee.getDepartmentCode()));
 
     DepartmentDTO departmentDTO = apiFeignClient.getDepartment(employee.getDepartmentCode());
 
